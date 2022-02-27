f416c639a445a12f320a1c5aff1fa0d9131eb90f
taniryla <ryland0000@gmail.com>
2022-02-27 09:06:05 -0700

    refactored app.js with hooks

--- src/components/users/User.js
+++ src/components/users/User.js
@@ -1,24 +1,16 @@
-import React, { Component, Fragment } from 'react';
+import React, { useEffect, useState, Fragment } from 'react';
 import Spinner from '../layout/Spinner';
 import Repos from '../repos/Repos';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 
-export class User extends Component {
-  componentDidMount() {
-    this.props.getUser(this.props.match.params.login)
-    this.props.getUserRepos(this.props.match.params.login)
-  }
+const User = ( { user, getUser, getUserRepos, repos, match, loading }) => {
+  useEffect(() => {
+    getUser(match.params.login);
+    getUserRepos(match.params.login);
+    // eslint-disable-next-line
+  }, []);
 
-  static propTypes = {
-      loading: PropTypes.bool,
-      user: PropTypes.object.isRequired,
-      repos: PropTypes.array.isRequired,
-      getUser: PropTypes.func.isRequired,
-      getUserRepos: PropTypes.func.isRequired
-  }
-
-  render() {
     const {
         avatar_url,
         login,
@@ -28,9 +20,8 @@ export class User extends Component {
         repos_url,
         gists_url,
         hireable
-    } = this.props.user;
+    } = user;
 
-    const { loading, repos } = this.props;
 
     if (loading) return <Spinner />;
 
@@ -69,6 +60,7 @@ export class User extends Component {
       </Fragment>
     )
   }
-}
+
+
 
 export default User
